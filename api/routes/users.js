29a54b49f10966e4617c44a791ef53811de3a01f
Muhammad <muhammad124711@gmail.com>
2025-02-12 15:48:54 +0500

    third route

--- api/routes/users.js
+++ api/routes/users.js
@@ -81,8 +81,24 @@ router.post('/login', async (req, res) => {
 // Dashboard data
 
 router.get('/all', async (req, res)=>{
-    //
-})
-
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Authorization header is missing' });
+    }
+    
+    const token = authHeader.split(" ")[1]; // Get the token after "Bearer"
+    if (!token) {
+        return res.status(401).json({ message: 'Token is missing' });
+    }
+    
+    jwt.verify(token, process.env.JWT_SECRET, (err) => {
+        if (err) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    
+        // Token is valid
+        res.status(200).json({total_projects: 15, total_clients: 5, total_developers: 8, total_leads: 4});
+    });
+});
 
 module.exports = router;
